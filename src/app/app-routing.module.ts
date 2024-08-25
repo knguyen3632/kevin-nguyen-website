e6e33d3368f2ace61c6286e9716cea8c86bd37f8
Kevin Nguyen <knguyen3632@gmail.com>
2024-08-25 14:51:13 -0400

    Added Dropdown menu for Projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { ContactComponent } from './contact/contact.component';
+import { WeatherProjectComponent } from './projects/weather-project/weather-project.component';
+import { JobTrackerProjectComponent } from './projects/job-tracker-project/job-tracker-project.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'work-experience', component: WorkExperienceComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'weather-app', component: WeatherProjectComponent },
+  { path: 'job-tracker-app', component: JobTrackerProjectComponent },
 ];
 
 @NgModule({
